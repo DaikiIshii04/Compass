1bbdf71b9a9b573bb9decd67c2292e64a1adec06
isiidaiki <d.nattu24@gmail.com>
2023-04-13 18:59:31 +0900

    リクエストになります

---
+++ app/Http/Requests/BulletinBoard/EditorialPostFormRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditorialPost extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        'post_title' => 'required|min:4|max:100',
+        'post_body' => 'required|min:10|max:5000',
+        ];
+    }
+        public function messages()
+    {
+        return [
+            'post_title.required' => 'タイトルを記入してください。',
+            'post_title.min' => 'タイトルは4文字以上記入してください。',
+            'post_title.max' => 'タイトルは100文字以内にしてください。',
+            'post_body.required' => '投稿内容を記入してください。',
+            'post_body.min' => '投稿内容は10文字以上記入してください。',
+            'post_body.max' => '投稿内容を500文字以内にしてください。',
+        ];
+    }
+}
---
+++ app/Http/Requests/MainCategoryFormRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MainCategoryFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            'main_category_name' => 'required|max:100|unique:main_categories,main_category'
+        ];
+    }
+        public function messages()
+    {
+        return [
+            'main_category_name.required' => 'メインカテゴリーを記入してください',
+            'main_category_name.unique' => 'すでに登録されています'
+        ];
+    }
+}
